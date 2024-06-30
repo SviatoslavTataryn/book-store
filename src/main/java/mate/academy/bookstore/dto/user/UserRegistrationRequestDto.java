a9f66c3bb4462d95d4c089b27150ff78f7543525
sviatoslavtataryn <www.t.svyatoslav55@gmail.com>
2024-06-30 21:35:22 +0200

    - Add User entity; - Add the required DTOs and mapper; - Add AuthenticationController; - Add FieldMatch annotation; - Add GlobalExceptionHandler.

---
+++ src/main/java/mate/academy/bookstore/dto/user/UserRegistrationRequestDto.java
@@ -0,0 +1,28 @@
+package mate.academy.bookstore.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import mate.academy.bookstore.validation.FieldMatch;
+import org.hibernate.validator.constraints.Length;
+
+@FieldMatch(first = "password", second = "repeatPassword", message = "Passwords must match")
+public record UserRegistrationRequestDto(
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Length(min = 8, max = 35)
+        String password,
+        @NotBlank
+        @Length(min = 8, max = 35)
+        String repeatPassword,
+        @NotNull
+        String firstName,
+        @NotNull
+        String lastName,
+        @NotNull
+        @Length(min = 5, max = 100)
+        String shippingAddress
+) {
+}
---
+++ src/main/java/mate/academy/bookstore/validation/FieldMatch.java
@@ -0,0 +1,20 @@
+package mate.academy.bookstore.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordValidator.class)
+
+public @interface FieldMatch {
+    String message() default "The password fields must match";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    String first();
+    String second();
+}
