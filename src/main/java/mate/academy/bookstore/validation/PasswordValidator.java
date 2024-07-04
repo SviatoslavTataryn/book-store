35a4a7d0acd1d5148491bcda37a89f2fefdcdbb0
sviatoslavtataryn <www.t.svyatoslav55@gmail.com>
2024-07-04 08:11:29 +0200

    fixed User und PasswordValidate

--- src/main/java/mate/academy/bookstore/validation/PasswordValidator.java
+++ src/main/java/mate/academy/bookstore/validation/PasswordValidator.java
@@ -3,18 +3,22 @@ package mate.academy.bookstore.validation;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import java.util.Objects;
+import org.springframework.beans.BeanWrapperImpl;
 
 public class PasswordValidator implements ConstraintValidator<FieldMatch, Object> {
+    private String firstFieldName;
+    private String secondFieldName;
 
     @Override
     public void initialize(FieldMatch constraintAnnotation) {
-        String firstObj = constraintAnnotation.first();
-        String secondObj = constraintAnnotation.second();
+        this.firstFieldName = constraintAnnotation.first();
+        this.secondFieldName = constraintAnnotation.second();
     }
 
     @Override
-    public boolean isValid(final Object firstObj,
-                           final ConstraintValidatorContext secondObj) {
+    public boolean isValid(final Object value, final ConstraintValidatorContext context) {
+        final Object firstObj = new BeanWrapperImpl(value).getPropertyValue(firstFieldName);
+        final Object secondObj = new BeanWrapperImpl(value).getPropertyValue(secondFieldName);
         return Objects.equals(firstObj, secondObj);
     }
 }
